6a837010ba7ebaea03006f5e8b92a52f065295eb
KUBOTA Atsushi <kubota@hiroshima-cu.ac.jp>
2021-05-11 16:34:30 +0900

    Optimized version of ocl_base added

---
+++ cg/ocl_ell_lf2/cg_float_app_kernels/src/cg.hpp
@@ -0,0 +1,68 @@
+#ifndef __INCLUDED_CG_HPP__
+#define __INCLUDED_CG_HPP__
+
+#include "sparse_defs.hpp"
+#include "sparse_lib.hpp"
+#include <cmath>
+#include <limits>     // std::numeric_limits
+#include <algorithm>  // std::max
+#include <iostream>
+
+#define FIXEDSIZE
+
+template <typename T>
+void
+solve
+(int ell_col[N][NZ],
+ T ell_val[N][NZ],
+ T b[N],
+ T x[N],
+ int max_iter,
+ int & iter,
+ T tol_error,
+ T & ret_error,
+ T p[N],
+ T q[N],
+ T r[N]
+ )
+{
+  T rho_old, rho_new, alpha, beta, rhsNorm2, resNorm2;
+  T tol = tol_error;
+
+  rhsNorm2 = dot<T>(b, b);
+  for (int i=0; i<N; i++) {
+    x[i] = (T)0;
+    r[i] = b[i];
+    p[i] = b[i];
+  }
+  rho_new = rhsNorm2; // assume x=0
+
+  alpha = rho_new/spmv_ell_dot<T>(ell_col, ell_val, p, q);
+  axpy2<T>(alpha, p, x, q, r);
+  resNorm2 = dot<T>(r, r);
+
+#ifndef FIXEDSIZE
+  // cf. Eigen/src/IterativeSolver/ConjugateGradient.h
+  const T considerAsZero = (std::numeric_limits<T>::min)();
+  const T threshold = std::max<T>(T(tol*tol*rhsNorm2), considerAsZero);
+#endif
+
+#ifdef FIXEDSIZE
+  for (iter=1; iter<NITER; iter++) {
+#else
+  for (iter=1; (iter<max_iter)&&(resNorm2>threshold); iter++) {
+#endif
+
+    rho_old = rho_new;
+    rho_new = resNorm2;
+    beta = rho_new/rho_old;
+    axpy<T>(beta, p, r, p);
+    alpha = rho_new/spmv_ell_dot<T>(ell_col, ell_val, p, q);
+    axpy2<T>(alpha, p, x, q, r);
+    resNorm2 = dot<T>(r, r);
+  }
+  ret_error = rho_new; // calculate sqrt(rho_new / rhsNorm2) in call site
+  return;
+}
+  
+#endif
---
+++ cg/ocl_ell_lf2/cg_float_app_kernels/src/kernel.cpp
@@ -0,0 +1,51 @@
+#include "sparse_defs.hpp"
+#include "cg.hpp"
+
+extern "C" {
+
+static int   kernel_ell_col[N][NZ];
+static float kernel_ell_val[N][NZ];
+static float kernel_b[N];
+static float kernel_x[N];
+
+static float p[N];
+static float q[N];
+static float r[N];
+
+void kernel_func
+(int ell_col[N*NZ],
+ float ell_val[N*NZ],
+ float b[N],
+ float x[N],
+ int max_iter,
+ int iter[1],
+ float tol_error,
+ float ret_error[1]
+ )
+{
+  int tmp_iter;
+  float tmp_error;
+  for (int i=0; i<N; i++) {
+    for (int j=0; j<NZ; j++) {
+      kernel_ell_col[i][j] = ell_col[i*NZ+j];
+      kernel_ell_val[i][j] = ell_val[i*NZ+j];
+    }    
+  }
+  for (int i=0; i<N; i++) {
+    kernel_b[i] = b[i];
+  }
+  solve<float>
+    (kernel_ell_col, kernel_ell_val,
+     kernel_b, kernel_x,
+     max_iter, tmp_iter,
+     tol_error, tmp_error,
+     p, q, r);
+  iter[0] = tmp_iter;
+  ret_error[0] = tmp_error;
+  for (int i=0; i<N; i++) {
+    x[i] = kernel_x[i];
+  }
+  return;
+}
+
+} /* end of extern C */
